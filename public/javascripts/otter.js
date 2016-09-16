04271325285222d17519abcbd05124d5adf8d043
ravenusmc <mcuddy77@gmail.com>
2016-09-16 16:11:17 -0400

    added javascript

--- public/javascripts/otter.js
+++ public/javascripts/otter.js
@@ -1,3 +1,9 @@
-function test(){
-  alert("hello");
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+  'use strict'
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', '/images/otter3.jpg');
 }
