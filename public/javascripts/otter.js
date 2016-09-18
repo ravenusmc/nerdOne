b4eed2d25ce54327d28ff7da5dd69dee9580fefd
ravenusmc <mcuddy77@gmail.com>
2016-09-18 15:56:16 -0400

    minor changes

--- public/javascripts/otter.js
+++ public/javascripts/otter.js
@@ -2,13 +2,13 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
-function setDetails() {
+function setDetails(imageUrl, titleText) {
   'use strict'
   //Changing the image
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', '/images/otter3.jpg');
+  detailImage.setAttribute('src', 'imageUrl');
 
   //Changing the image title
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-  detailTitle.textContent = "You Should be Dancing";
+  detailTitle.textContent = titleText;
 }
