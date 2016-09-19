99d7857bbb6350472ebb02a566c0c89ab6f44942
ravenusmc <mcuddy77@gmail.com>
2016-09-19 16:20:35 -0400

    Added more javascript functions

--- public/javascripts/otter.js
+++ public/javascripts/otter.js
@@ -6,9 +6,33 @@ function setDetails(imageUrl, titleText) {
   'use strict'
   //Changing the image
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', 'imageUrl');
+  detailImage.setAttribute('src', imageUrl);
 
   //Changing the image title
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   detailTitle.textContent = titleText;
+}
+
+// setDetails('/images/otter4.jpg', 'Night Fever')
+function imageFromThumb(thumbnail){
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail){
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail){
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb){
+  'use strict';
+  thumb.addEventListener('click', function(event){
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  })
 }
